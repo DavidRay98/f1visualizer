9e4070e46b327afff30330de718654b1bc813610
David Sugar <sugdave9.8@gmail.com>
2023-03-12 14:05:45 +0100

    szia

--- f1visualizer.py
+++ f1visualizer.py
@@ -1,1 +1,10 @@
 print("hello dave")
+
+import urllib.request
+import json
+
+url = 'https://ergast.com/api/f1/2023/drivers.json'
+result = json.load(urllib.request.urlopen(url))
+result["MRData"]
+
+
