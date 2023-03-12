d4f8baa35efa24a00335bf4abaab9483a538bf94
Tamas Nagy <nredline@gmail.com>
2023-03-12 14:15:41 +0100

    Get drivers array

--- f1visualizer.py
+++ f1visualizer.py
@@ -1,10 +1,12 @@
-print("hello dave")
-
 import urllib.request
 import json
 
 url = 'https://ergast.com/api/f1/2023/drivers.json'
 result = json.load(urllib.request.urlopen(url))
-result["MRData"]
 
+driver_table = result['MRData']['DriverTable']
 
+season = driver_table['season']
+drivers = driver_table['Drivers']
+
+print(drivers)
