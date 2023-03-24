10e6e1366c6ff3d1b05ab02ef16e2d628a1ced1d
David Sugar <sugdave9.8@gmail.com>
2023-03-24 16:59:40 +0100

    félsiker

--- f1visualizer.py
+++ f1visualizer.py
@@ -21,19 +21,18 @@ race_table = result['MRData']['RaceTable']
 season = race_table['season']
 round =  race_table['round']
 races =  race_table['Races']
-year = races['season']
-weekend = races['round']
-weekendname = races['raceName']
+year = races[0]['season']
+weekend = races[0]['round']
+weekendname = races[0]['raceName']
 print(weekendname)
 
-# A kövi blokkban tuti elcsesztem a struktúrát, mert belekavarodok a szintekbe 
-results = races['Results']
-pos = results['position']
+results = races[0]['Results']
 podium = results[0:3]
-for happy in podium:
-   print(happy['position'],happy['Driver']['FamilyName'],happy['points'])
+for pos in podium:
+   print(pos['position'],'.',pos['Driver']['givenName'],pos['Driver']['familyName'],pos['points'],"points")
 
+#print(results[0]['position'],'.',results[0]['Driver']['givenName'],results[0]['Driver']['familyName'],results[0]['points'],"points")
+#print(results[1]['position'],'.',results[1]['Driver']['givenName'],results[1]['Driver']['familyName'],results[1]['points'],"points")
+p#rint(results[2]['position'],'.',results[2]['Driver']['givenName'],results[2]['Driver']['familyName'],results[2]['points'],"points")
 
-#Így most majdnem az összes sort feldolgoztam. Erre szükség van, vagy lehet kapásból úgy belépni mint az mrdata - > racetable esetében?
-#valahogy biztos lehetne szebben, hogy bekérni számot és akkor az annyiadik futam adatait kiírni, de ehhez azért még kevés vagyok
-
+#
