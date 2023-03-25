88d4c2c17be877db494c2f3ca9fe1de9b141dd31
David Sugar <sugdave9.8@gmail.com>
2023-03-25 17:48:18 +0100

    javított

--- f1visualizer.py
+++ f1visualizer.py
@@ -17,10 +17,7 @@ import json
 url2 = 'https://ergast.com/api/f1/2021/5/results.json'
 result = json.load(urllib.request.urlopen(url2))
 
-race_table = result['MRData']['RaceTable']    
-season = race_table['season']
-round =  race_table['round']
-races =  race_table['Races']
+races =  result['MRData']['RaceTable']['Races']
 year = races[0]['season']
 weekend = races[0]['round']
 weekendname = races[0]['raceName']
@@ -33,6 +30,4 @@ for pos in podium:
 
 #print(results[0]['position'],'.',results[0]['Driver']['givenName'],results[0]['Driver']['familyName'],results[0]['points'],"points")
 #print(results[1]['position'],'.',results[1]['Driver']['givenName'],results[1]['Driver']['familyName'],results[1]['points'],"points")
-p#rint(results[2]['position'],'.',results[2]['Driver']['givenName'],results[2]['Driver']['familyName'],results[2]['points'],"points")
-
-#
+#print(results[2]['position'],'.',results[2]['Driver']['givenName'],results[2]['Driver']['familyName'],results[2]['points'],"points")
