c9bc864fe2b5cb4fce7ab28271e1cb72b1013ad0
Tamas Nagy <nredline@gmail.com>
2023-03-25 17:40:00 +0100

    Add new driverStandings based processing

---
+++ f1visualizer_t.py
@@ -0,0 +1,26 @@
+import urllib.request
+import json
+
+
+has_data = True
+round = 1
+
+while has_data:
+    url = f'https://ergast.com/api/f1/2023/{round}/driverStandings.json'
+    result = json.load(urllib.request.urlopen(url))
+    data = result['MRData']
+
+    has_data = int(data['total']) > 0
+
+    if not has_data:
+      break
+
+    driver_standings = data['StandingsTable']['StandingsLists'][0]['DriverStandings']    
+    
+    round = round + 1
+
+#print(results[0]['position'],'.',results[0]['Driver']['givenName'],results[0]['Driver']['familyName'],results[0]['points'],"points")
+#print(results[1]['position'],'.',results[1]['Driver']['givenName'],results[1]['Driver']['familyName'],results[1]['points'],"points")
+#print(results[2]['position'],'.',results[2]['Driver']['givenName'],results[2]['Driver']['familyName'],results[2]['points'],"points")
+
+
