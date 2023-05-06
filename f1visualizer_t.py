2a2d936cc633e38a44ef0ee27ef8c42731b59f30
Tamas Nagy <nredline@gmail.com>
2023-05-06 20:46:42 +0200

    Provide option to select drivers and year

--- f1visualizer_t.py
+++ f1visualizer_t.py
@@ -1,6 +1,24 @@
 import urllib.request
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 import json
+import sys
+
+
+year_input = input("Add meg az évet: ")
+
+if not year_input.isnumeric():
+  print("Az év nem szám!")
+  exit(1)
+
+year = int(year_input)
+
+if year not in range(1970,2024):
+  print("Ebben az évben nem rendeztek F1-et!")
+  exit(1)
+
+drivers_input = input("Add meg a sofőröket vesszővel elválasztva: ")
+
+drivers = drivers_input.split(",")
 
 has_data = True
 round = 1
@@ -8,8 +26,11 @@ round = 1
 standings_dict_initialized = False
 standings_dict = { }
 
+for driver in drivers:
+  standings_dict[driver] = []
+
 while has_data:
-    url = f'https://ergast.com/api/f1/2021/{round}/driverStandings.json'
+    url = f'https://ergast.com/api/f1/{year}/{round}/driverStandings.json'
     result = json.load(urllib.request.urlopen(url))
     data = result['MRData']
 
@@ -19,34 +40,25 @@ while has_data:
       break
 
     driver_standings = data['StandingsTable']['StandingsLists'][0]['DriverStandings']
-
-    if not standings_dict_initialized:
-      for standing in driver_standings:
-        driver_code = standing['Driver']['code']
-        standings_dict[driver_code] = []
-
-      standings_dict_initialized = True
     
     for standing in driver_standings:
       driver_code = standing['Driver']['code']
       points = float(standing['points'])
 
-      if driver_code not in standings_dict:
-        standings_dict[driver_code] = []
-        
-      standings_dict[driver_code].append(points)
+      if driver_code in standings_dict:    
+        standings_dict[driver_code].append(points)
       
     round = round + 1
 
 
-activity = [*range(1, 23, 1)]
+activity = [*range(1, round, 1)]
 
-fig, ax = plt.subplots()
+figure, axes = plot.subplots()
 
 for key in standings_dict:
   if len(standings_dict[key]) == len(activity):
-    ax.plot(activity, standings_dict[key], label=key)
+    axes.plot(activity, standings_dict[key], label=key)
 
 
-ax.legend()
-plt.show()
+axes.legend()
+plot.show()
